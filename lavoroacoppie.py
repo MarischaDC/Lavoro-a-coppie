0f09b00d361edf6fb08acff779cb7b4f3b52de14
GretaUcelli <greta.ucelli@gmail.com>
2024-10-30 15:27:32 +0100

    Update lavoroacoppie.py

--- lavoroacoppie.py
+++ lavoroacoppie.py
@@ -1,19 +1,20 @@
-import 
 class FileReader:
-    def __init__(self, data_txt):
-        self.data_txt = 'data.txt'
+    def __init__(self, file_path):
+        self.file_path = file_path
 
     def read_file(self):
         try:
             with open(self.file_path, 'r') as file:
-                content = file.read()
+                content = file.readlines() 
             return content
         except FileNotFoundError:
-            return "File non trovato."
+            return ["File non trovato."]
         except IOError:
-            return "Errore durante la lettura del file."
+            return ["Errore durante la lettura del file."]
 
-# Utilizzo della classe
-file_reader = FileReader('percorso/del/tuo/file.txt')
+file_reader = FileReader('data.txt')
 contenuto = file_reader.read_file()
-print(contenuto)
+
+for riga in contenuto:
+    print(riga.strip())  
+

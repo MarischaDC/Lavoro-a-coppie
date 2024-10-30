17bee33dea91f555dbd0d0eb3658dfa79406afeb
GretaUcelli <greta.ucelli@gmail.com>
2024-10-30 15:20:13 +0100

    Update lavoroacoppie.py

--- lavoroacoppie.py
+++ lavoroacoppie.py
@@ -1,1 +1,19 @@
 import 
+class FileReader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def read_file(self):
+        try:
+            with open(self.file_path, 'r') as file:
+                content = file.read()
+            return content
+        except FileNotFoundError:
+            return "File non trovato."
+        except IOError:
+            return "Errore durante la lettura del file."
+
+# Utilizzo della classe
+file_reader = FileReader('percorso/del/tuo/file.txt')
+contenuto = file_reader.read_file()
+print(contenuto)
